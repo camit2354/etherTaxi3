2ac54a5b9e30bf4bf5c2ea4222f2fff5e9ee0230
camit2354 <camit2354@gmail.com>
2022-03-20 11:52:29 +0530

    change of index,js file

--- pages/index.js
+++ pages/index.js
@@ -1,120 +1,79 @@
-import React, { Component } from "react";
-import { Card , Button , Container } from "semantic-ui-react";
-import 'semantic-ui-css/semantic.min.css'
-import mainServer from "../ethereum/mainServer";
+import React , {Component} from 'react'
+import {
+    Menu,
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Search,
+  Segment,
+} from 'semantic-ui-react'
+
+import UserLogin from '../components/userLogin';
+import DriverLogin from '../components/driverLogin';
+
+import 'semantic-ui-css/semantic.min.css';
+
+import web3 from '../ethereum/web3' ;
 import {Link} from '../routes' ;
+import mainServer from "../ethereum/mainServer";
 
-class MainServer extends Component {
-  
-  static async getInitialProps() {
-    const users = await mainServer.methods.getUsers().call();
-    const drivers = await mainServer.methods.getDrivers().call();
-    const ridePlatformInstances = await mainServer.methods.getRidePlatformInstances().call() ;
-    
-    let userProfiles = [] ;
-    let driverProfiles = [] ;
 
-    for(let user of users)
-    {
-      const userP = await mainServer.methods.userProfile(user).call() ;
-      userProfiles.push(userP) ;
+class Home extends Component{
+
+   state = {
+       activeItem : 'user'
+   }
+
+   handleItemClick = (e, { name }) =>{
+    this.setState({ activeItem: name })
     }
 
-    for(let driver of drivers)
-    {
-      const driverP = await mainServer.methods.driverProfile(driver).call() ;
-      driverProfiles.push(driverP) ;
+    renderLoginPage(){
+        const {activeItem} = this.state ;
+        if(activeItem == 'user')
+        {
+            return (
+                <UserLogin/>
+            );
+        }
+        else 
+        {
+            return(
+                <DriverLogin/>
+            );
+        }
     }
 
-    return {users,drivers,userProfiles,driverProfiles,ridePlatformInstances } ;
-  }
+    render(){
 
-  renderUsers(){
-    const items = this.props.userProfiles.map(address =>{
-      return {
-        header : address ,
-        description : (
-        <Link route = {`profile/user/${address}`}>
-        <a>Login</a>
-        </Link> ) ,
-        fluid : true 
-      } ;
-    }) ;
+        const {activeItem} = this.state ;
+        return (           
 
-    return <Card.Group items = {items} />
+            <Container>
+                <Menu attached='top' tabular>
+                    <Menu.Item
+                        name='user'
+                        active={activeItem === 'user'}
+                        onClick={this.handleItemClick}
+                    />
+                    <Menu.Item
+                        name='driver'
+                        active={activeItem === 'driver'}
+                        onClick={this.handleItemClick}
+                    />
+                </Menu>
+                <Segment placeholder>
+                    {this.renderLoginPage()}
+                </Segment>
+                   
+            </Container> 
+        ) ;
+        }
+    }
 
-  }
-
-  renderDrivers(){
-    const items = this.props.driverProfiles.map(address =>{
-      return {
-        header : address ,
-        description : (
-                <Link route = {`/profile/driver/${address}`} >
-                <a>Login</a>
-                </Link>  ) ,
-        fluid : true 
-      } ;
-    }) ;
-
-    return <Card.Group items = {items} />
-
-  }
-
-  renderRidePlatformInstances(){
-    const items = this.props.ridePlatformInstances.map(address =>{
-      return {
-        header : address ,
-        description : <a>Login</a> ,
-        fluid : true 
-      } ;
-    }) ;
-
-    return <Card.Group items = {items} />
-
-  }
-
-  render() {
-    return (
-    
-    <Container>
-      <h1>Main server home page !</h1>
-
-      <Link route = "/register/user">
-      <a>
-        <Button
-          content = "User Register"
-          icon = "add circle"
-          floated = "right"
-          primary />
-
-      </a>
-      </Link>
-     
-      <Link route = "/register/driver">
-      <a>
-      <Button
-        content = "Driver Register"
-        icon = "add circle"
-        floated = "right"
-        primary />
-         </a>
-      </Link>
-
-      <h2>users :</h2>
-      <div>{this.renderUsers()}</div>
-      
-
-      <h2>Drivers : </h2>
-      <div>{this.renderDrivers()}</div>
-
-      <h2>ride platform instances :</h2>
-      <div>{this.renderRidePlatformInstances()}</div>
-      
-    </Container>
-      );
-  }
-}
+    export default Home ;
 
 
-export default MainServer;
